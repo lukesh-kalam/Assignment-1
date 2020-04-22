ecf5cb63c38498c0c0f8e6ab94a1e4356041ccfa
lukesh-kalam <63307165+lukesh-kalam@users.noreply.github.com>
2020-04-22 07:53:19 +0530

    case-2 is working for negative values also

--- case-2/2.cpp
+++ case-2/2.cpp
@@ -1,8 +1,8 @@
 /*
 filename: 2.cpp
-details:To find the formula for given expression :-- 1+4+9+16+….+100
-author:K>Lukesh Kumar
-date:09/04/2020
+details:To find the formula for given expression :-- 1+4+9+16+â€¦.+100
+author:K.Lukesh Kumar
+date:21/04/2020
 */
 #include<iostream>
 #include<stdlib.h>
@@ -11,7 +11,7 @@ using namespace std;
 //main function
 int main(int argc,char *argv[])
 {
-    int Sum,Num; //declaring  variables
+    int Sum,Num,a; //declaring  variables
     if(argc==1) 
 	{
 		cout<<"\n Usage of file --> \n"
@@ -22,18 +22,45 @@ int main(int argc,char *argv[])
 		cout<<"Enter Input: ";
 		cin>>Num; //read 'Num'
 		Sum=0;
-    	for(int i=1; i<=Num; i++)	//checks the  condition
-        	Sum =Sum + (i*i);	//returns the output based on the logic to : 1+4+9+16+….+100
-     	cout << "Output: " << Sum;
+		if(Num<0)
+		{
+		    a=Num*(-1);
+    	for(int i=1; i<=a; i++)	//checks the  condition
+        	Sum =Sum + (i*i);//returns the output based on the logic to : 1+4+9+16+â€¦.+100
+     	cout << "Output: " << Sum*(-1);
      	return 0;
+		}
+		else
+	    {
+	       for(int i=1; i<=a; i++)	//checks the  condition
+        	Sum =Sum + (i*i);//returns the output based on the logic to : 1+4+9+16+â€¦.+100
+     	cout << "Output: " << Sum;
+     	return 0; 
+	    }
+     	
 	}
 	else
 	{	
-		Num = atoi(argv[1]); //assigning the argument to 'n'
-    	Sum=0;
-    	for(int i=1; i<=Num; i++)	//checks the argument value
-        	Sum =Sum + (i*i);	//gives output based on the expression: 1+4+9+16+….+100
-     	cout << "Output: " << Sum;
+	    	cout<<"Enter Input: ";
+		cin>>Num; //read 'Num'
+		Sum=0;
+		if(Num<0)
+		{
+		    a=Num*(-1);
+    	for(int i=1; i<=a; i++)	//checks the  condition
+        	Sum =Sum + (i*i);//returns the output based on the logic to : 1+4+9+16+â€¦.+100
+     	cout << "Output: " << Sum*(-1);
      	return 0;
+		}
+		else
+	    {
+	       for(int i=1; i<=a; i++)	//checks the  condition
+        	Sum =Sum + (i*i);//returns the output based on the logic to : 1+4+9+16+â€¦.+100
+     	cout << "Output: " << Sum;
+     	return 0; 
+	    }
+	    
+	    
+	
 	} 
 }
